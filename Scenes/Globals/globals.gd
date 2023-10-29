dd2dd3fa4b0d49d9bb488964c46f2549851aa612
Pablo Terradillos <tehsis@gmail.com>
2023-10-28 22:38:10 -0300

    wip

---
+++ Scenes/Globals/globals.gd
@@ -0,0 +1,8 @@
+extends Node
+
+var player = {
+    "position": Vector2(0,0),
+    "has_jetpack": false,
+    "has_gun": false,
+    "jetpack_energy": 100
+}
--- Scenes/Player/Player.gd
+++ Scenes/Player/Player.gd
@@ -6,9 +6,12 @@ const JUMP_VELOCITY = -50.0
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+@export var velocityFallingThreshold: int = 500
+
 var have_gun = false
 var have_jetpack = false
 var facing_to: Vector2 = Vector2.RIGHT
+var isFalling = false
 
 # Input signals
 signal shoot_laser(pos: Vector2)
@@ -21,10 +24,16 @@ func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if velocity.y > velocityFallingThreshold:
+			isFalling = true
+	elif isFalling:
+		isFalling = false
+		die()
 
 	# Handle Jump.
 	if Input.is_action_pressed("JUMP") and have_jetpack:
 		velocity.y += JUMP_VELOCITY
+		isFalling = false
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -45,3 +54,9 @@ func _on_gun_pickup(body):
 	if body.name == "Player":
 		$"..".remove_child($"Gun")
 		have_gun = true
+
+func die():
+	$AnimationPlayer.play("die")
+	await $AnimationPlayer.animation_finished
+	await get_tree().create_timer(1.0).timeout
+	get_tree().reload_current_scene()
