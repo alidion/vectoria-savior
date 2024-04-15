f4d82154b00a004d2f69286caea41c5f041254af
Pablo Terradillos <tehsis@gmail.com>
2024-04-14 22:50:00 -0300

    Fix bug path to re-start inverted

--- Scenes/Enemies/Bug.gd
+++ Scenes/Enemies/Bug.gd
@@ -9,24 +9,28 @@ var SPEED = 500.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 var positions: Array
-var next_position_index = 0
+var inverted_positions: Array
+
+var next_position
 
 func _ready():
 	update_configuration_warnings()
 	var markers = $Positions.get_children()
 	for marker in markers:
-		positions.append(marker.global_position)	
+		positions.append(marker.global_position)
+	next_position = positions.pop_front()
 
 func _physics_process(_delta):
 	if not Engine.is_editor_hint():
-		if positions.size() > 0:
-			var current_next_position = positions[next_position_index]
+		if position.distance_to(next_position) < 5:
+			inverted_positions.push_front(next_position)
+			if positions.size() == 0:
+				positions = inverted_positions
+				inverted_positions = []
+			next_position = positions.pop_front()
 
-			if position.distance_to(current_next_position) < 2:
-				next_position_index = (next_position_index + 1) % positions.size()
-
-			var direction = (current_next_position - global_position).normalized()
-			velocity = direction * 500
+		var direction = global_position.direction_to(next_position)
+		velocity = direction * SPEED
 
 		move_and_slide()
 
